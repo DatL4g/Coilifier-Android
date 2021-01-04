50e03874da20263d913870a0446cbbba65403aaf
DatLag <46448715+DATL4G@users.noreply.github.com>
2021-01-04 16:35:27 +0100

    added some builder functions, removed load extensions from context

--- coilifier/src/main/java/de/datlag/coilifier/Coilifier.kt
+++ coilifier/src/main/java/de/datlag/coilifier/Coilifier.kt
@@ -13,8 +13,11 @@ import androidx.fragment.app.FragmentActivity
 import com.bumptech.glide.RequestBuilder
 import com.bumptech.glide.TransitionOptions
 import com.bumptech.glide.load.Transformation
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.DownsampleStrategy
 import com.bumptech.glide.load.resource.gif.GifDrawable
 import com.bumptech.glide.request.BaseRequestOptions
+import com.bumptech.glide.request.RequestListener
 import de.datlag.coilifier.commons.getBitmap
 import java.io.File
 
@@ -22,14 +25,31 @@ data class Coilifier<ResourceType> internal constructor(
     @DrawableRes val errorResId: Int?,
     val errorDrawable: Drawable?,
     val errorBitmap: Bitmap?,
+    val errorRequest: RequestBuilder<ResourceType>?,
     @DrawableRes val placeholderResId: Int?,
     val placeholderDrawable: Drawable?,
     val placeholderBitmap: Bitmap?,
+    @DrawableRes val fallbackResId: Int?,
+    val fallbackDrawable: Drawable?,
+    val fallbackBitmap: Bitmap?,
+    val requestListener: List<RequestListener<ResourceType>>,
     val thumbnailSizeMultiplier: Float?,
     val thumbnailRequestBuilder: List<RequestBuilder<ResourceType>>,
     val transitionOptions: TransitionOptions<*, in ResourceType?>?,
     val transformOptions: List<Transformation<Bitmap?>>,
     val scale: Scale?,
+    val overrideWidth: Int?,
+    val overrideHeight: Int?,
+    val downsampleStrategy: DownsampleStrategy?,
+    val encodeFormat: Bitmap.CompressFormat?,
+    val encodeQuality: Int?,
+    val dontAnimate: Boolean,
+    val dontTransform: Boolean,
+    val clone: Boolean,
+    val autoClone: Boolean,
+    val decodeClass: Class<*>?,
+    val diskCacheStrategy: DiskCacheStrategy?,
+    val disallowHardwareConfig: Boolean,
     val requestOptions: BaseRequestOptions<*>?
 ) {
 
@@ -38,12 +58,19 @@ data class Coilifier<ResourceType> internal constructor(
         private var errorResId: Int? = null
         private var errorDrawable: Drawable? = null
         private var errorBitmap: Bitmap? = null
+        private var errorRequest: RequestBuilder<ResourceType>? = null
 
         @DrawableRes
         private var placeholderResId: Int? = null
         private var placeholderDrawable: Drawable? = null
         private var placeholderBitmap: Bitmap? = null
 
+        @DrawableRes
+        private var fallbackResId: Int? = null
+        private var fallbackDrawable: Drawable? = null
+        private var fallbackBitmap: Bitmap? = null
+
+        private var requestListener: List<RequestListener<ResourceType>> = listOf()
         private var thumbnailSizeMultiplier: Float? = null
         private var thumbnailRequestBuilder: List<RequestBuilder<ResourceType>> = listOf()
 
@@ -51,12 +78,29 @@ data class Coilifier<ResourceType> internal constructor(
         private var transformOptions: List<Transformation<Bitmap?>> = listOf()
         private var scale: Scale? = null
 
+        private var overrideWidth: Int? = null
+        private var overrideHeight: Int? = null
+
+        private var downsampleStrategy: DownsampleStrategy? = null
+        private var encodeFormat: Bitmap.CompressFormat? = null
+        private var encodeQuality: Int? = null
+
+        private var dontAnimate: Boolean = false
+        private var dontTransform: Boolean = false
+        private var clone: Boolean = false
+        private var autoClone: Boolean = false
+
+        private var decodeClass: Class<*>? = null
+
+        private var diskCacheStrategy: DiskCacheStrategy? = null
+        private var disallowHardwareConfig: Boolean = false
         private var requestOptions: BaseRequestOptions<*>? = null
 
         fun error(@DrawableRes drawableResId: Int) = apply {
             this.errorResId = drawableResId
             this.errorDrawable = null
             this.errorBitmap = null
+            this.errorRequest = null
         }
 
         fun error(drawable: Drawable?) = apply {
@@ -66,6 +110,7 @@ data class Coilifier<ResourceType> internal constructor(
             this.errorDrawable = drawable
             this.errorResId = 0
             this.errorBitmap = null
+            this.errorRequest = null
         }
 
         fun error(bitmap: Bitmap?) = apply {
@@ -73,6 +118,7 @@ data class Coilifier<ResourceType> internal constructor(
             this.errorBitmap = bitmap
             this.errorDrawable = null
             this.errorResId = 0
+            this.errorRequest = null
         }
 
         fun error(view: View?) = when (view) {
@@ -88,11 +134,19 @@ data class Coilifier<ResourceType> internal constructor(
             else -> error(view.getBitmap())
         }
 
+        fun error(requestBuilder: RequestBuilder<ResourceType>) = apply {
+            this.errorResId = 0
+            this.errorDrawable = null
+            this.errorBitmap = null
+            this.errorRequest = requestBuilder
+        }
+
         fun error(any: Any?): Builder<ResourceType> = when (any) {
             is Int -> error(any)
             is Drawable -> error(any)
             is Bitmap -> error(any)
             is View -> error(any)
+            is RequestBuilder<*> -> error(any)
             null -> clearError()
             else -> this
         }
@@ -101,6 +155,7 @@ data class Coilifier<ResourceType> internal constructor(
             this.errorBitmap = null
             this.errorDrawable = null
             this.errorResId = 0
+            this.errorRequest = null
         }
 
         fun placeholder(@DrawableRes drawableResId: Int) = apply {
@@ -153,9 +208,65 @@ data class Coilifier<ResourceType> internal constructor(
             this.placeholderResId = 0
         }
 
+        fun fallback(@DrawableRes drawableResId: Int) = apply {
+            this.fallbackResId = drawableResId
+            this.fallbackDrawable = null
+            this.fallbackBitmap = null
+        }
+
+        fun fallback(drawable: Drawable?) = apply {
+            if (drawable is BitmapDrawable) {
+                if (drawable.bitmap.isRecycled) return@apply
+            }
+            this.fallbackDrawable = drawable
+            this.fallbackResId = 0
+            this.fallbackBitmap = null
+        }
+
+        fun fallback(bitmap: Bitmap?) = apply {
+            if (bitmap?.isRecycled == true) return@apply
+            this.fallbackBitmap = bitmap
+            this.fallbackResId = 0
+            this.fallbackDrawable = null
+        }
+
+        fun fallback(view: View?) = when(view) {
+            is ImageView -> {
+                val bitmap = view.getBitmap()
+                if (bitmap != null) {
+                    fallback(bitmap)
+                } else {
+                    fallback(view.drawable)
+                }
+            }
+            null -> clearFallback()
+            else -> fallback(view.getBitmap())
+        }
+
+        fun fallback(any: Any?) = when (any) {
+            is Int -> fallback(any)
+            is Drawable -> fallback(any)
+            is Bitmap -> fallback(any)
+            is View -> fallback(any)
+            null -> clearFallback()
+            else -> this
+        }
+
+        private fun clearFallback() = apply {
+            this.fallbackResId = 0
+            this.fallbackDrawable = null
+            this.fallbackBitmap = null
+        }
+
+        fun listener(vararg requestListener: RequestListener<ResourceType>) = apply {
+            this.requestListener = requestListener.toList()
+        }
+
         fun thumbnail(sizeMultiplier: Float) = apply {
             this.thumbnailSizeMultiplier = sizeMultiplier
         }
+
+        fun thumbnail(sizeMultiplier: Double) = thumbnail(sizeMultiplier.toFloat())
 
         fun thumbnail(vararg thumbnailRequestBuilder: RequestBuilder<ResourceType>) = apply {
             this.thumbnailRequestBuilder = thumbnailRequestBuilder.toList()
@@ -205,57 +316,141 @@ data class Coilifier<ResourceType> internal constructor(
             this.scale = Scale.OPTIONAL_CIRCLE_CROP
         }
 
+        fun override(width: Int, height: Int) = apply {
+            this.overrideWidth = width
+            this.overrideHeight = height
+        }
+
+        fun override(width: Long, height: Long) = override(width.toInt(), height.toInt())
+        fun override(width: Float, height: Float) = override(width.toInt(), height.toInt())
+        fun override(width: Double, height: Double) = override(width.toInt(), height.toInt())
+
+        fun override(size: Int) = override(size, size)
+        fun override(size: Long) = override(size, size)
+        fun override(size: Float) = override(size, size)
+        fun override(size: Double) = override(size, size)
+
+        fun downsample(strategy: DownsampleStrategy) = apply {
+            this.downsampleStrategy = strategy
+        }
+
+        fun encodeFormat(format: Bitmap.CompressFormat) = apply {
+            this.encodeFormat = format
+        }
+
+        fun encodeQuality(quality: Int) = apply {
+            this.encodeQuality = quality
+        }
+
+        fun encodeQuality(quality: Long) = encodeQuality(quality.toInt())
+        fun encodeQuality(quality: Float) = encodeQuality(quality.toInt())
+        fun encodeQuality(quality: Double) = encodeQuality(quality.toInt())
+
+        @JvmOverloads
+        fun animate(`do`: Boolean = true) = apply {
+            this.dontAnimate = !`do`
+        }
+
+        fun dontAnimate() = animate(false)
+
+        @JvmOverloads
+        fun transform(`do`: Boolean = true) = apply {
+            this.dontTransform = !`do`
+        }
+
+        fun dontTransform() = transform(false)
+
+        @JvmOverloads
+        fun clone(`do`: Boolean = true) = apply {
+            this.clone = `do`
+        }
+
+        @JvmOverloads
+        fun autoClone(`do`: Boolean = true) = apply {
+            this.autoClone = `do`
+        }
+
+        fun decode(resourceClass: Class<*>) = apply {
+            this.decodeClass = resourceClass
+        }
+
+        fun diskCacheStrategy(strategy: DiskCacheStrategy) = apply {
+            this.diskCacheStrategy = strategy
+        }
+
+        @JvmOverloads
+        fun hardwareConfig(allow: Boolean = true) = apply {
+            this.disallowHardwareConfig = !allow
+        }
+
+        fun disallowHardwareConfig() = hardwareConfig(false)
+
         fun apply(requestOptions: BaseRequestOptions<*>) = apply {
             this.requestOptions = requestOptions
         }
 
-        fun build(): Coilifier<ResourceType> {
-            return Coilifier(
+        internal fun build() = Coilifier(
                 errorResId,
                 errorDrawable,
                 errorBitmap,
+                errorRequest,
                 placeholderResId,
                 placeholderDrawable,
                 placeholderBitmap,
+                fallbackResId,
+                fallbackDrawable,
+                fallbackBitmap,
+                requestListener,
                 thumbnailSizeMultiplier,
                 thumbnailRequestBuilder,
                 transitionOptions,
                 transformOptions,
                 scale,
+                overrideWidth,
+                overrideHeight,
+                downsampleStrategy,
+                encodeFormat,
+                encodeQuality,
+                dontAnimate,
+                dontTransform,
+                clone,
+                autoClone,
+                decodeClass,
+                diskCacheStrategy,
+                disallowHardwareConfig,
                 requestOptions
-            )
-        }
+        )
     }
 
     companion object {
-        inline fun <reified ResourceType> with(context: Context): Loader<ResourceType> = Request<ResourceType>(context).`as`(ResourceType::class.java)
-        inline fun <reified ResourceType> with(activity: Activity): Loader<ResourceType> = Request<ResourceType>(activity).`as`(ResourceType::class.java)
-        inline fun <reified ResourceType> with(fragment: Fragment): Loader<ResourceType> = Request<ResourceType>(fragment).`as`(ResourceType::class.java)
-        inline fun <reified ResourceType> with(view: View): Loader<ResourceType> = Request<ResourceType>(view).`as`(ResourceType::class.java)
-        inline fun <reified ResourceType> with(fragmentActivity: FragmentActivity): Loader<ResourceType> = Request<ResourceType>(fragmentActivity).`as`(ResourceType::class.java)
+        @JvmStatic inline fun <reified ResourceType> with(context: Context): Loader<ResourceType> = Request<ResourceType>(context).`as`(ResourceType::class.java)
+        @JvmStatic inline fun <reified ResourceType> with(activity: Activity): Loader<ResourceType> = Request<ResourceType>(activity).`as`(ResourceType::class.java)
+        @JvmStatic inline fun <reified ResourceType> with(fragment: Fragment): Loader<ResourceType> = Request<ResourceType>(fragment).`as`(ResourceType::class.java)
+        @JvmStatic inline fun <reified ResourceType> with(view: View): Loader<ResourceType> = Request<ResourceType>(view).`as`(ResourceType::class.java)
+        @JvmStatic inline fun <reified ResourceType> with(fragmentActivity: FragmentActivity): Loader<ResourceType> = Request<ResourceType>(fragmentActivity).`as`(ResourceType::class.java)
 
-        fun withAsDrawable(context: Context): Loader<Drawable> = Request<Drawable>(context).asDrawable()
-        fun withAsDrawable(activity: Activity): Loader<Drawable> = Request<Drawable>(activity).asDrawable()
-        fun withAsDrawable(fragment: Fragment): Loader<Drawable> = Request<Drawable>(fragment).asDrawable()
-        fun withAsDrawable(view: View): Loader<Drawable> = Request<Drawable>(view).asDrawable()
-        fun withAsDrawable(fragmentActivity: FragmentActivity): Loader<Drawable> = Request<Drawable>(fragmentActivity).asDrawable()
+        @JvmStatic fun withAsDrawable(context: Context): Loader<Drawable> = Request<Drawable>(context).asDrawable()
+        @JvmStatic fun withAsDrawable(activity: Activity): Loader<Drawable> = Request<Drawable>(activity).asDrawable()
+        @JvmStatic fun withAsDrawable(fragment: Fragment): Loader<Drawable> = Request<Drawable>(fragment).asDrawable()
+        @JvmStatic fun withAsDrawable(view: View): Loader<Drawable> = Request<Drawable>(view).asDrawable()
+        @JvmStatic fun withAsDrawable(fragmentActivity: FragmentActivity): Loader<Drawable> = Request<Drawable>(fragmentActivity).asDrawable()
 
-        fun withAsBitmap(context: Context): Loader<Bitmap> = Request<Bitmap>(context).asBitmap()
-        fun withAsBitmap(activity: Activity): Loader<Bitmap> = Request<Bitmap>(activity).asBitmap()
-        fun withAsBitmap(fragment: Fragment): Loader<Bitmap> = Request<Bitmap>(fragment).asBitmap()
-        fun withAsBitmap(view: View): Loader<Bitmap> = Request<Bitmap>(view).asBitmap()
-        fun withAsBitmap(fragmentActivity: FragmentActivity): Loader<Bitmap> = Request<Bitmap>(fragmentActivity).asBitmap()
+        @JvmStatic fun withAsBitmap(context: Context): Loader<Bitmap> = Request<Bitmap>(context).asBitmap()
+        @JvmStatic fun withAsBitmap(activity: Activity): Loader<Bitmap> = Request<Bitmap>(activity).asBitmap()
+        @JvmStatic fun withAsBitmap(fragment: Fragment): Loader<Bitmap> = Request<Bitmap>(fragment).asBitmap()
+        @JvmStatic fun withAsBitmap(view: View): Loader<Bitmap> = Request<Bitmap>(view).asBitmap()
+        @JvmStatic fun withAsBitmap(fragmentActivity: FragmentActivity): Loader<Bitmap> = Request<Bitmap>(fragmentActivity).asBitmap()
 
-        fun withAsFile(context: Context): Loader<File> = Request<File>(context).asFile()
-        fun withAsFile(activity: Activity): Loader<File> = Request<File>(activity).asFile()
-        fun withAsFile(fragment: Fragment): Loader<File> = Request<File>(fragment).asFile()
-        fun withAsFile(view: View): Loader<File> = Request<File>(view).asFile()
-        fun withAsFile(fragmentActivity: FragmentActivity): Loader<File> = Request<File>(fragmentActivity).asFile()
+        @JvmStatic fun withAsFile(context: Context): Loader<File> = Request<File>(context).asFile()
+        @JvmStatic fun withAsFile(activity: Activity): Loader<File> = Request<File>(activity).asFile()
+        @JvmStatic fun withAsFile(fragment: Fragment): Loader<File> = Request<File>(fragment).asFile()
+        @JvmStatic fun withAsFile(view: View): Loader<File> = Request<File>(view).asFile()
+        @JvmStatic fun withAsFile(fragmentActivity: FragmentActivity): Loader<File> = Request<File>(fragmentActivity).asFile()
 
-        fun withAsGif(context: Context): Loader<GifDrawable> = Request<GifDrawable>(context).asGif()
-        fun withAsGif(activity: Activity): Loader<GifDrawable> = Request<GifDrawable>(activity).asGif()
-        fun withAsGif(fragment: Fragment): Loader<GifDrawable> = Request<GifDrawable>(fragment).asGif()
-        fun withAsGif(view: View): Loader<GifDrawable> = Request<GifDrawable>(view).asGif()
-        fun withAsGif(fragmentActivity: FragmentActivity): Loader<GifDrawable> = Request<File>(fragmentActivity).asGif()
+        @JvmStatic fun withAsGif(context: Context): Loader<GifDrawable> = Request<GifDrawable>(context).asGif()
+        @JvmStatic fun withAsGif(activity: Activity): Loader<GifDrawable> = Request<GifDrawable>(activity).asGif()
+        @JvmStatic fun withAsGif(fragment: Fragment): Loader<GifDrawable> = Request<GifDrawable>(fragment).asGif()
+        @JvmStatic fun withAsGif(view: View): Loader<GifDrawable> = Request<GifDrawable>(view).asGif()
+        @JvmStatic fun withAsGif(fragmentActivity: FragmentActivity): Loader<GifDrawable> = Request<File>(fragmentActivity).asGif()
     }
 }
--- coilifier/src/main/java/de/datlag/coilifier/Loader.kt
+++ coilifier/src/main/java/de/datlag/coilifier/Loader.kt
@@ -41,25 +41,35 @@ class Loader<ResourceType>(
 
     private fun loadData(helper: Coilifier<ResourceType>) {
         appliedRequestBuilder.apply {
+            if (helper.disallowHardwareConfig) {
+                disallowHardwareConfig()
+            }
             when {
                 helper.errorResId != null && helper.errorResId != 0 -> error(helper.errorResId)
                 helper.errorDrawable != null -> error(helper.errorDrawable)
                 helper.errorBitmap != null -> error(BitmapDrawable(resources, helper.errorBitmap))
+                helper.errorRequest != null -> error(helper.errorRequest)
             }
             when {
                 helper.placeholderResId != null && helper.placeholderResId != 0 -> placeholder(helper.placeholderResId)
                 helper.placeholderDrawable != null -> placeholder(helper.placeholderDrawable)
                 helper.placeholderBitmap != null -> placeholder(BitmapDrawable(resources, helper.placeholderBitmap))
             }
-            if (helper.thumbnailSizeMultiplier != null) {
-                thumbnail(helper.thumbnailSizeMultiplier)
+            when {
+                helper.fallbackResId != null && helper.fallbackResId != 0 -> fallback(helper.fallbackResId)
+                helper.fallbackDrawable != null -> fallback(helper.fallbackDrawable)
+                helper.fallbackBitmap != null -> fallback(BitmapDrawable(resources, helper.fallbackBitmap))
             }
+            if (helper.requestListener.isNotEmpty()) {
+                helper.requestListener.forEach {
+                    addListener(it)
+                }
+            }
+            helper.thumbnailSizeMultiplier?.let { thumbnail(it) }
             if (helper.thumbnailRequestBuilder.isNotEmpty()) {
                 thumbnail(*helper.thumbnailRequestBuilder.toTypedArray())
             }
-            if (helper.transitionOptions != null) {
-                transition(helper.transitionOptions)
-            }
+            helper.transitionOptions?.let { transition(it) }
             when (helper.scale) {
                 Scale.FIT_CENTER -> fitCenter()
                 Scale.CENTER_CROP -> centerCrop()
@@ -70,7 +80,28 @@ class Loader<ResourceType>(
                 Scale.OPTIONAL_CENTER_INSIDE -> optionalCenterInside()
                 Scale.OPTIONAL_CIRCLE_CROP -> optionalCircleCrop()
             }
+            if (helper.overrideWidth != null && helper.overrideHeight != null) {
+                override(helper.overrideWidth, helper.overrideHeight)
+            }
+            helper.downsampleStrategy?.let { downsample(it) }
+            helper.encodeFormat?.let { encodeFormat(it) }
+            helper.encodeQuality?.let { encodeQuality(it) }
+            if (helper.dontAnimate) {
+                dontAnimate()
+            }
+            if (helper.dontTransform) {
+                dontTransform()
+            }
             transform(*helper.transformOptions.toTypedArray())
+            if (helper.clone) {
+                clone()
+            }
+            if (helper.autoClone) {
+                autoClone()
+            }
+            if (helper.decodeClass != null) {
+                decode(helper.decodeClass)
+            }
         }
     }
 
@@ -85,5 +116,11 @@ class Loader<ResourceType>(
     fun preload() = appliedRequestBuilder.preload()
 
     fun submit() = appliedRequestBuilder.submit()
+
+    fun clone() = appliedRequestBuilder.clone()
+
+    fun autoClone() = appliedRequestBuilder.autoClone()
+
+    fun decode(resourceClass: Class<*>) = appliedRequestBuilder.decode(resourceClass)
 
 }
